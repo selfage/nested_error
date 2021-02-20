4698d3e68a51951575ccb3f93871a8aa657b11db
Yuxuan Xie <teststaybaka@gmail.com>
2021-02-20 07:08:14 +0000

    Release

--- error.ts
+++ error.ts
@@ -3,6 +3,7 @@ import { CustomError } from "ts-custom-error";
 export class NestedError extends CustomError {
   public constructor(message?: string, public cause?: any) {
     super(message);
+    // this.name = '123123';
 
     // stack is a non-standard property though supported in most cases.
     if (this.stack && this.cause) {
